1a1b27b2e812a2a1cc84e0def9ca976d33cf25b4
sivakrishnagarnepudi <gsivakrishna007@gmail.com>
2024-01-11 10:47:39 +0530

    Initial commit

---
+++ src/test/java/com/Neoteric/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Neoteric.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
